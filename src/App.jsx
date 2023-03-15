eaf0931c1d37e75af67967dbeef2218d08dc7cf9
Leon Nguyen <vicnguyenvuong@gmail.com>
2023-03-15 12:51:11 +0700

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Home from "./page/Home";
+import Home from "./page/home";
 import { Sepolia } from "@thirdweb-dev/chains";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 function App() {
